ea08f80d240479a3bb618d1b0b42051e238a86ae
tubasa2911 <randoctor11@gmail.com>
2024-12-11 13:46:32 +0900

    作成機能とバリデーションを実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,4 +18,23 @@ class PostController extends Controller {
         // 変数$postをposts/show.blade.phpファイルに渡す
         return view('posts.show', compact('post'));
         }
+
+        public function create() {
+            return view('posts.create');
+        }
+        public function store(Request $request) {
+            // バリデーションを設定
+            $request->validate([
+                'title' => 'required|max:20',
+                'content' => 'required|max:200'
+            ]);
+            // フォームの入力内容をもとに、テーブルにデータを追加す
+            $post = new Post();
+            $post->title = $request->input('title');
+            $post->content = $request->input('content');
+            $post->save();
+
+            // レイダイレクトする
+            return redirect("/posts");
+        }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
